9045d084fc1b5cf578f6816402b7d52d0255299e
redzz <164974345+redzgt@users.noreply.github.com>
2024-12-09 08:13:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://mathsspot.com';
+const nggUrl = 'https://bonk.io';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
